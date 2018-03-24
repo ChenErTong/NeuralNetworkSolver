5366d1c9f7e7d3498e39a250287b01124d0093de
Hanifor <141250016@smail.nju.edu.cn>
2018-03-24 18:21:42 +0800

    finish logics

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,20 +2,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-//        NetParser parser = new NetParser();
-//        List<Solution> solutions = parser.parse(Utility.WEIGHT_PATH, Utility.BIAS_PATH);
-//
-//        InequalitiesSolver solver = InequalitiesSolver.instance;
-//        solver.showResult("X1^2 + X2^3 < 2");
-//        for (Solution solution: solutions) {
-//            String result = solver.solveInequalities(solution.getInput_number(), solution.getConstraint());
-//            if(!result.equals("Impossible")){
-//                System.out.println(solution.getConstraint());
-//                System.out.println(result);
-//                System.out.println();
-//            }
-//        }
-//
-//        solver.close();
+
     }
 }
